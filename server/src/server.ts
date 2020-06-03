764a68f2c7322ce6062511495114dafda5674a71
manogalo <johnnycampos@outlook.com>
2020-06-02 21:34:52 -0300

    feat: método GET para retornar o nome de um usuário único

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,15 +7,36 @@ const app = express();
 // PUT: Atualizar uma informação existente no back-end
 // DELETE: Remover uma informação do back-end
 
+// Request.params = Parâmetros enviados na própria rota que identificam um recurso
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
+
 app.get('/users',(request, response) => {
     console.log('listagem de usuários');
 
-    response.send([
-        'Diego',
-        'Clayton',
-        'Robson',
-        'Johnny'
-    ]);
+    response.send(users);
+});
+
+app.get('/user/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    };
+
+    return response.json(users);
 });
 
 app.listen(3333);
