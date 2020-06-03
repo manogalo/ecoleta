33a8736a72b40929b18ee5b211cd351505d4ec33
manogalo <johnnycampos@outlook.com>
2020-06-02 21:12:31 -0300

    feat: adicionado rascunho dos métodos HTTP

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
 app.get('/users',(request, response) => {
     console.log('listagem de usuários');
 
