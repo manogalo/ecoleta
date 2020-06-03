eae0036d9469155a9494df6cc5330c660941ca2f
manogalo <johnnycampos@outlook.com>
2020-06-02 21:48:28 -0300

    feat: adicionado request body na requisição post

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 // GET: Buscar uma ou mais informações do back-end
 // POST: Criar uma nova informação no back-end
 // PUT: Atualizar uma informação existente no back-end
@@ -9,6 +11,7 @@ const app = express();
 
 // Request params = Parâmetros enviados na própria rota que identificam um recurso
 // Query params = Parâmetros enviados na própria rota, geralmente opcionais, para filtros, paginação, etc (separados por "?")
+// Request body = Parâmetros para criação e atualização de informações enviados no corpo da requisição
 
 const users = [
     'Diego',
@@ -34,12 +37,14 @@ app.get('/user/:id', (request, response) => {
 });
 
 app.post('/users', (request, response) => {
-    const user = {
-        name: 'Diego',
-        email: 'diego@rocketseat.com.br'
-    };
+    const data = request.body
 
-    return response.json(users);
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
