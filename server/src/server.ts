5cacd52e427034bf1245da9d630d00fb2924107c
manogalo <johnnycampos@outlook.com>
2020-06-03 22:42:12 -0300

    fix:corrigido diretório temp

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/temp', express.static(path.resolve(__dirname, '..', 'temp')));
 
 app.listen(3333);
