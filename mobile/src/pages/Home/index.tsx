5ee4c675d0d2ce78adc251df3c4a6f6ef2f38936
manogalo <johnnycampos@outlook.com>
2020-06-07 13:16:52 -0300

    feat:implementada navegação entre as telas

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const navigation = useNavigation();
+
+    function handleNavigateToPoints() {
+        navigation.navigate('Points');
+    }
+
     return (
     <ImageBackground 
         style={styles.container} 
@@ -17,7 +24,7 @@ const Home = () => {
         </View>
 
         <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={handleNavigateToPoints}>
                 <View style={styles.buttonIcon}>
                     <Text>
                         <Icon name="arrow-right" color="#fff" size={24} />
@@ -32,8 +39,7 @@ const Home = () => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      padding: 32,
-      backgroundColor: '#f0f0f5'
+      padding: 32
     },
   
     main: {
