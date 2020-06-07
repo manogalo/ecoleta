8d286e2305e4675533eef1b01c298babd657d9b1
manogalo <johnnycampos@outlook.com>
2020-06-07 15:41:19 -0300

    feat:buscando itens da API

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image } from 'react-native'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import {SvgUri} from 'react-native-svg'
+import api from '../../services/api'
+
+interface Item {
+  id: number,
+  name: string,
+  image_url: string
+}
 
 const Points = () => {
     const navigation = useNavigation();
+
+    const [items, setItems] = useState<Item[]>([]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    useEffect(()=> {
+      api.get('items')
+      .then(response => 
+        setItems(response.data)
+      )
+    }, [])
 
     function handleNavigateBack() {
         navigation.goBack();
@@ -15,6 +32,17 @@ const Points = () => {
     
     function handleNavigateToDetail() {
         navigation.navigate('Detail');
+    }
+
+    function handleSelectItem(id: number) {
+      const alreadySelected = selectedItems.findIndex(item => item === id)
+
+      if (alreadySelected >= 0) {
+          const filteredItems = selectedItems.filter(item => item !== id)
+          setSelectedItems(filteredItems);
+      } else {
+          setSelectedItems([...selectedItems, id]);
+      }
     }
 
     return (
@@ -62,30 +90,20 @@ const Points = () => {
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={{ paddingHorizontal: 20 }}
                 >
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                  {items.map(item => (
+                    <TouchableOpacity 
+                      key={String(item.id)} 
+                      style={[
+                        styles.item,
+                        selectedItems.includes(item.id) ? styles.selectedItem : {}
+                      ]}
+                      activeOpacity={0.6}
+                      onPress={() => handleSelectItem(item.id)}
+                    >
+                      <SvgUri width={42} height={42} uri={item.image_url}></SvgUri>
+                      <Text style={styles.itemTitle}>{item.name}</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.18:3333/temp/lampadas.svg"></SvgUri>
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
+                  ))}
                 </ScrollView>
             </View>
         </>
